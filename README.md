90ab8ab32fbe5e6fb5b22775220f2811dc4c17a2
Luis <luisirigoin123@gmail.com>
2022-09-07 20:48:48 -0500

    cambios css styles

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ## Minijuego piola
-Este es un pequeño proyecto que hize para empezar en react[Podes jugralo aqui](https://galaxym4.github.io/memory-game/)
+Este es un pequeño proyecto que hize para empezar en react
+[Podes jugralo aqui](https://galaxym4.github.io/memory-game/)
 
 ## Antes de jugar:
-El juego es muy simple, se te va a mostar por un cierto tiempo una tabla con frutas en su interior, tu deberás **MEMORIZARLAS**, luego la tabla va a cambiar y tu deberas seleccionar donde estuvo cierta fruta en la tabla, es mi primer proyecto ya se que esta feoy cutre xd. 
+El juego es muy simple, se te va a mostar por un cierto tiempo una tabla con frutas en su interior, tu deberás **MEMORIZARLAS**, luego la tabla va a cambiar y tu deberas seleccionar donde estuvo cierta fruta en la tabla, es mi primer proyecto ya se que esta feoy cutre xd.
--- src/index.tsx
+++ src/index.tsx
@@ -43,22 +43,29 @@ function get_play_functions(max_id: number) {
 
     return obj_return;
 }
+
 ///Renderizando las dificultades
-root.render(<div>
+root.render(
+<div>
     <h2 id="title">Selecciona dificultad</h2>
     <div id="buttons_dif">
         <button className="dif_button" onClick={dif_0}>Facil</button>
         <button className="dif_button" onClick={dif_1}>Medio</button>
         <button className="dif_button" onClick={dif_2}>Dificil</button>
-    </div>
-    
+    </div>    
 </div>);
 
 game.on("inicio", async(stats) => {
     update_dimensions();
-    root.render(<div>
-        <h2 id="title">Juego piola</h2>
-        <div id="score">Score: {stats.score}       Level: {stats.level}</div>
+    root.render(
+    <div id="main">
+        <h2 id="title">Primer nivel</h2>
+        <div id="info">
+            <ul>
+                <li>Score: {stats.score}</li>
+                <li>Level: {stats.level}</li>
+            </ul>
+        </div>
         <div id="board">{get_board(largo,ancho,stats.f_ids)}</div>
         <div id="question"></div>
     </div>);
@@ -66,21 +73,34 @@ game.on("inicio", async(stats) => {
 
 game.on("ask", async(stats) =>{
     update_dimensions();
-    console.log(stats.table);
     let play_f = get_play_functions(largo*ancho);
-    root.render(<div>
-        <h2 id="title">Juego piola</h2>
-        <div id="score">Score: {stats.score}       Level: {stats.level}</div>
+    root.render(
+    <div id="main">
+        <h2 id="title">Responde...</h2>
+        <div id="info">
+            <ul>
+                <li>Score: {stats.score}</li>
+                <li>Level: {stats.level}</li>
+            </ul>
+        </div>
         <div id="board">{get_board(largo,ancho, get_obj_buttons(largo,ancho, play_f))}</div>
         <div id="question">Donde esta {stats.ask}?</div>
     </div>);
 });
 
 game.on("correcto", async (stats) => {
+    var correct_titles = ["BIEN", "CORRECTO", "K GRANDE", "PIOLA", "TA BIEN", "EXELENTE", "EPICO"];
+    var title = correct_titles[Math.floor(Math.random()*(correct_titles.length))];
     update_dimensions();
-    root.render(<div>
-        <h2 id="title">Juego piola</h2>
-        <div id="score">Score: {stats.score}       Level: {stats.level}  CORRECTO!</div>
+    root.render(
+    <div id="main">
+        <h2 id="title_c">{title}</h2>
+        <div id="info">
+            <ul>
+                <li>Score: {stats.score}</li>
+                <li>Level: {stats.level}</li>
+            </ul>
+        </div>
         <div id="board">{get_board(largo,ancho, stats.f_ids)}</div>
         <div id="question">Memorizar</div>
     </div>);
@@ -88,11 +108,19 @@ game.on("correcto", async (stats) => {
 
 game.on("perdida", async (stats) => {
     update_dimensions();
-    root.render(<div>
-        <h2 id="title">FIN DEL JUEGO</h2>
-        <div id="score">Final Score: {stats.score}</div>
+    var no_correct_titles = ["PERDISTE JAJA", "FIN DEL JUEGO", "PARTIDA TERMINADA", "PERDISTE NUV", "MAL, YA PERDISTE"];
+    var title = no_correct_titles[Math.floor(Math.random()*(no_correct_titles.length))];
+    root.render(
+    <div id="main">
+        <h2 id="title">{title}</h2>
+        <div id="info">
+            <ul>
+                <li>Final Score: {stats.score}</li>
+                <li>Level: {stats.level}</li>
+            </ul>
+        </div>
         <div id="board">{get_board(largo,ancho, stats.f_ids)}</div>
-        <div id="question">Respuestas: {stats.rptas.join(", ")}</div>
+        <div id="question">Respuestas Correctas: {stats.rptas.join(", ")}</div>
     </div>);
 });
 reportWebVitals();
